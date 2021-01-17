0386ab963245f9bba09f129989384233b1c25f69
Rania <rann.attia@gmail.com>
2021-01-17 09:23:12 +0100

    ReadMe and requirements

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# DataCamp_EEG_DATA
+# RAMP starting kit on classification EEG signals
+
+## Description
+
+This project is based on EEGs signals. Electroencephalography is a monitoring method to record electrical activity of the brain. Non-invasive electrodes are placed on different locations on the scalp and the signals are retrieved. 
+
+The goal of this challenge is to be able to predict a seen digit (from 0 to 9) based on the signals recorded from the brain of a subject who saw the same digit, and so is thinking about it. Which leads to the name of our dataset "The MNIST of Brain Digits". 
+
+It originally contains over a million brain signals, of 2 seconds each captured with the stimulus of seeing a digit form 0 to 9. These signals were captured using 4 different EEGs devices, NeuroSky Mindwave, emotiv EPOC, Interaxon Muse and Emotiv Insight, covering a total of 19 brain locations.
+
+This is a multi classifications challenge, with a total of 11 classes: -1, 0, ... , 9.
+## Preparing data
+
+- The original "MindBigData" is split into 4 seperate files, with multiple lines describing one experience. To prepare the data in the desired format that we eventually worked with, it had to be aggregated, shuffled and split into testing and training set. The [prepare_data.py script](prepare_data.py) does that for you, if you have the original zipped folders and wish to try things out. If not, we already ran this and the resulting csv files will be found in the [data](/data) folder.
+
+- The [problem.py script](problem.py) is then used to combine the data from each device, then again shuffle the row to get maximum randomness. It is responsible for loading the final data onto your notebook, as well as connecting to the Ramp server.
+
+## Get started
+
+- The [starting_kit](mind_data_starting_kit.ipynb) provides explanations, steps as well as our final [estimator](/submissions/starting_kit/estimator.py).
+
+- To run test this estimator on your side, simply open a terminal in this folder and launch the command:
+
+```bash
+ramp-test --submissions starting_kit
+```
+
+You can then replace the [starting_kit folder](/submissions/starting_kit) with your own folder containing your estimator.
+
+- Make sure to download all the packages necessary if you wish to run things locally. You can do so by simply running 
+
+```bash
+pip install -U -r requirements.txt
+```
+
+## Further details
+
+
+If you have any problems with the Ramp command, you can use this for help:
+
+```bash
+ramp-test --help
+```
+
+- If you wish to know more about how the Ramp works, more detailes can be found [here](https://paris-saclay-cds.github.io/ramp-docs/ramp-workflow/stable/using_kits.html)
--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -1,8 +1,5 @@
-import warnings
-warnings.filterwarnings("ignore")
-
 import numpy as np
-from sklearn.preprocessing import FunctionTransformer, OrdinalEncoder, OneHotEncoder
+from sklearn.preprocessing import FunctionTransformer, OneHotEncoder
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
@@ -10,6 +7,9 @@ from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import GradientBoostingClassifier
 from numpy.fft import fft, fftfreq
 import pywt
+import warnings
+warnings.filterwarnings("ignore")
+
 
 class DemocracyEstimator(BaseEstimator, ClassifierMixin):
     def __init__(self):
@@ -21,7 +21,7 @@ class DemocracyEstimator(BaseEstimator, ClassifierMixin):
         self.transformers = {}
         self.imputers = {}
         self.pipelines = {}
-        
+
         for device in self.devices:
             self.estimators[device] = GradientBoostingClassifier()
             self.transformers[device] = ColumnTransformer(
@@ -40,63 +40,57 @@ class DemocracyEstimator(BaseEstimator, ClassifierMixin):
     def fit(self, X, y):
         X_dict = {}
         y_dict = {}
-        
+
         for key in self.estimators.keys():
             X_dict[key] = X.loc[X['device'] == key]
             y_dict[key] = y[X['device'] == key]
 
-        
         for key in self.estimators.keys():
-            print("Fitting estimator of device: ",key, "with df_train.shape=",X_dict[key].shape)
-            """ self.transformers[key].fit(X_dict[key])
-            transformed_samples = self.transformers[key].transform(X_dict[key])
-            try:
-                self.estimators[key].fit(transformed_samples, y_dict[key])
-            except:
-                print(transformed_samples) """
+            print("Fitting estimator of device: ", key,
+                  "with df_train.shape=", X_dict[key].shape)
 
             self.pipelines[key].fit(X_dict[key], y_dict[key])
 
-        print("Fitting phase finished...")    
+        print("Fitting phase finished...")
         return self
 
     def predict(self, X):
         y_pred = np.empty((X.shape[0]))
-        
+
         for k in range(X.shape[0]):
             row = X.loc[[k], :]
             device = row.loc[k, "device"]
 
             y_pred[k] = self.pipelines[device].predict(row)
-        
+
         return y_pred
 
     def predict_proba(self, X):
         prob_pred = np.empty((X.shape[0], 11))
         prob_pred[:] = np.nan
         n = X.shape[0]
-        
-        if n == 7144 :
+
+        if n == 7144:
             step = "Computing train score:"
-        
-        else : 
+
+        else:
             step = "Computing cv score:"
-        
+
         for k in range(n):
             row = X.loc[[k], :]
             device = row.loc[k, "device"]
 
             aux = self.pipelines[device].predict_proba(row)[0]
-            
+
             if aux.size == 11:
                 prob_pred[k, :] = aux
-            
+
             else:
                 prob_pred[k, :] = np.concatenate(([0], aux))
-                
-            if k%500 == 0:
-                print(step,k,"/",n)
-                
+
+            if k % 500 == 0:
+                print(step, k, "/", n)
+
         return np.array(prob_pred)
 
     def score(self, X, y):
@@ -106,32 +100,30 @@ class DemocracyEstimator(BaseEstimator, ClassifierMixin):
 
 def feature_extractor(X):
     X = X.reset_index(drop=True)
-    n_channels = len(X.loc[0,"signals"])
+    n_channels = len(X.loc[0, "signals"])
     X_df = X['signals']
     n_freq = 2
     n_features = 10
     feature_array = np.zeros((len(X_df), n_channels*n_features))
-    
+
     for k, (_, x) in enumerate(X_df.iteritems()):
         len_list = len(x[0])
         for i in range(n_channels):
             # Wavelet
-            (ca, cd) = pywt.dwt(x[i],'haar')
+            (ca, cd) = pywt.dwt(x[i], 'haar')
             cat = pywt.threshold(ca, np.std(ca)/2)
             cdt = pywt.threshold(cd, np.std(cd)/2)
-            wv_feat =list()
+            wv_feat = list()
             wv_feat.append(np.mean(cat))
             wv_feat.append(np.max(cat))
             wv_feat.append(np.min(cat))
-
 
             wv_feat.append(np.mean(cdt))
             wv_feat.append(np.max(cdt))
             wv_feat.append(np.min(cdt))
 
-
             wv_feat = np.array(wv_feat)
-            
+
             # FFT
             ft = fft(x[i])
             freqs_ft = fftfreq(len_list)
@@ -141,10 +133,11 @@ def feature_extractor(X):
             freqs = freqs_ft[indices]
             fft_feat = np.concatenate(
                 (freqs, np.mean(x[i]).reshape(-1), np.std(x[i]).reshape(-1)))
-            
+
             # Concat
-            feature_array[k, (n_features*i):((n_features*(i+1)))] = np.concatenate((fft_feat,wv_feat))
-            
+            feature_array[k, (n_features*i):((n_features*(i+1)))] = \
+                np.concatenate((fft_feat, wv_feat))
+
     return feature_array
 
 
